e7a45cdfe4db3aef830f7473d258bdaa1fed3e24
Marcus R. Brown <me@igetgam.es>
2020-12-13 03:40:27 -0700

    chore: add eslint-config-amex

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require('@oclif/command');
 
 class TypedecCommand extends Command {
   async run() {
-    const {flags} = this.parse(TypedecCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.js`)
+    const { flags: { name = 'world' } } = this.parse(TypedecCommand);
+    this.log(`hello ${name} from ./src/index.js`);
   }
 }
 
 TypedecCommand.description = `Describe the command here
 ...
 Extra documentation goes here
-`
+`;
 
 TypedecCommand.flags = {
   // add --version flag to show CLI version
-  version: flags.version({char: 'v'}),
+  version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
-  help: flags.help({char: 'h'}),
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+  help: flags.help({ char: 'h' }),
+  name: flags.string({ char: 'n', description: 'name to print' }),
+};
 
-module.exports = TypedecCommand
+module.exports = TypedecCommand;
